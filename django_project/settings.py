5039e61032c302505c6bb7f4ad3b4ceccc934e60
OmegaHawkeye <chornitzerj@gmail.com>
2021-04-19 14:01:28 +0200

    Changed success url after login

--- django_project/settings.py
+++ django_project/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = "landing"
+LOGIN_REDIRECT_URL = "home"
 LOGIN_URL = 'login'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
