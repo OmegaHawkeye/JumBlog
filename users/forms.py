73b8df2bde0dcec801e423d9d6cba355f597dcfd
OmegaHawkeye <chornitzerj@gmail.com>
2021-04-20 09:53:36 +0200

    Added SignUp Functionallity

--- users/forms.py
+++ users/forms.py
@@ -16,10 +16,10 @@ class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
 
-        for fieldname in ['username','email','password1']:
+        for fieldname in ['username','email','password1','password2']:
             self.fields[fieldname].help_text = None
             self.fields[fieldname].label = ""
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1']
+        fields = ['username', 'email', 'password1','password2']
